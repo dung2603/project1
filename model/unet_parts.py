4782e63d0e9a5d7c418c18445aeb4291026e7e9c
dung2603 <137010724+dung2603@users.noreply.github.com>
2024-11-18 00:02:25 +0900

    Update unet_parts.py

--- model/unet_parts.py
+++ model/unet_parts.py
@@ -6,60 +6,65 @@ import torch.nn.functional as F
 
 
 class DoubleConv(nn.Module):
-    """(convolution => [BN] => ReLU) * 2 hoặc (convolution => ReLU) * 2 tùy thuộc vào `use_bn`"""
+    """(convolution => [BN] => ReLU) * 2"""
 
-    def __init__(self, in_channels, out_channels, mid_channels=None, use_bn=True):
+    def __init__(self, in_channels, out_channels, mid_channels=None):
         super().__init__()
         if not mid_channels:
             mid_channels = out_channels
-
-        layers = []
-        layers.append(nn.Conv2d(in_channels, mid_channels, kernel_size=3, padding=1))
-        if use_bn:
-            layers.append(nn.BatchNorm2d(mid_channels))
-        layers.append(nn.ReLU(inplace=True))
-
-        layers.append(nn.Conv2d(mid_channels, out_channels, kernel_size=3, padding=1))
-        if use_bn:
-            layers.append(nn.BatchNorm2d(out_channels))
-        layers.append(nn.ReLU(inplace=True))
-
-        self.double_conv = nn.Sequential(*layers)
+        self.double_conv = nn.Sequential(
+            nn.Conv2d(in_channels, mid_channels, kernel_size=3, padding=1, bias=False),
+            nn.BatchNorm2d(mid_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(mid_channels, out_channels, kernel_size=3, padding=1, bias=False),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True)
+        )
 
     def forward(self, x):
         return self.double_conv(x)
 
 
+class Down(nn.Module):
+    """Downscaling with maxpool then double conv"""
+
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.maxpool_conv = nn.Sequential(
+            nn.MaxPool2d(2),
+            DoubleConv(in_channels, out_channels)
+        )
+
+    def forward(self, x):
+        return self.maxpool_conv(x)
+
+
 class Up(nn.Module):
     """Upscaling then double conv"""
 
-    def __init__(self, in_channels, out_channels, bilinear=True, use_bn=True):
+    def __init__(self, in_channels, out_channels, bilinear=True):
         super().__init__()
-        self.use_bn = use_bn
 
+        # if bilinear, use the normal convolutions to reduce the number of channels
         if bilinear:
             self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
-            # mid_channels = in_channels // 2
-            self.conv = DoubleConv(in_channels, out_channels, use_bn=use_bn)
+            self.conv = DoubleConv(in_channels, out_channels, in_channels // 2)
         else:
             self.up = nn.ConvTranspose2d(in_channels, in_channels // 2, kernel_size=2, stride=2)
-            self.conv = DoubleConv(in_channels, out_channels, use_bn=use_bn)
+            self.conv = DoubleConv(in_channels, out_channels)
 
     def forward(self, x1, x2):
         x1 = self.up(x1)
+        # input is CHW
+        diffY = x2.size()[2] - x1.size()[2]
+        diffX = x2.size()[3] - x1.size()[3]
 
-        if x2 is not None:
-            # Xử lý kích thước nếu cần
-            diffY = x2.size()[2] - x1.size()[2]
-            diffX = x2.size()[3] - x1.size()[3]
-
-            x1 = F.pad(x1, [diffX // 2, diffX - diffX // 2,
-                            diffY // 2, diffY - diffY // 2])
-
-            x = torch.cat([x2, x1], dim=1)
-        else:
-            x = x1
-
+        x1 = F.pad(x1, [diffX // 2, diffX - diffX // 2,
+                        diffY // 2, diffY - diffY // 2])
+        # if you have padding issues, see
+        # https://github.com/HaiyongJiang/U-Net-Pytorch-Unstructured-Buggy/commit/0e854509c2cea854e247a9c615f175f76fbb2e3a
+        # https://github.com/xiaopeng-liao/Pytorch-UNet/commit/8ebac70e633bac59fc22bb5195e513d5832fb3bd
+        x = torch.cat([x2, x1], dim=1)
         return self.conv(x)
 
 
